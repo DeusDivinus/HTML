a80298a593743083091257efbfabc9217fb3e10b
Daniel <78000390+DeusDivinus@users.noreply.github.com>
2022-11-26 13:49:15 -0600

    Add final project

---
+++ Web Development/Bompton Studio/js/homepage.js
@@ -0,0 +1,22 @@
+var header = document.querySelector("header")
+
+window.onscroll = function() {
+    // Change the opacity of the header if scrolled down
+    window.pageYOffset <= 0 ? styleManipulator(header, "opacity", 1): styleManipulator(header, "opacity", 0.6);
+    this.oldScroll > this.scrollY 
+    ? styleManipulator(header, "top", `${0}px`) & styleManipulator(header, "box-shadow", getStyleElement(header, "--box-shadow"))
+    : styleManipulator(header, "top", `${-150}px`) & styleManipulator(header, "box-shadow", `none`)
+
+    this.oldScroll = this.scrollY;
+};
+
+function styleManipulator(element, property, value)
+{
+    element.style.setProperty(property, value)
+}
+function getStyleElement(element, property, number)
+{
+    style = getComputedStyle(element).getPropertyValue(property);
+    number ? style = style.match(/.\d*/)[0] : style;
+    return style
+}
