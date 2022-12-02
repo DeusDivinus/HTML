f6c96484de281ca4f9bcb6a8fc3a1f76cd5c28c2
Daniel <78000390+DeusDivinus@users.noreply.github.com>
2022-12-02 00:56:45 -0600

    Add files via upload

--- Web Development/Bompton Studio/js/homepage.js
+++ Web Development/Bompton Studio/js/homepage.js
@@ -1,8 +1,10 @@
+galleryIMG = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '7.jpg', '8.jpg']
+
 var header = document.querySelector("header")
 var oldScroll = window.scrollY
 var state = false;
 
-
+// Header
 window.onscroll = function() {
     // Change the opacity of the header if scrolled down
     window.pageYOffset <= 0 ? styleManipulator(header, "opacity", 1): styleManipulator(header, "opacity", 0.6);
@@ -13,6 +15,33 @@ window.onscroll = function() {
     
 };
 
+// Overlay
+
+function toggleOverlay()
+{
+    styleManipulator(document.querySelector(".overlay"), "display", "flex")
+}
+
+function previewIMG(img)
+{
+    pattern = /\d.jpg/g;
+    toggleOverlay()
+    try { 
+    console.log(pattern.exec(img)[0])
+    } catch (error) {
+    }
+}
+
+// Gallery
+function addImages()
+{
+    galleryIMG.forEach(element => {
+        document.querySelector('.galery-content').innerHTML +=
+        ` <div class="g-imageholder"><img 
+            src="../img/tattoos/gallery/thumbnail/${element}" onclick="previewIMG(this.src)"></div>
+        `
+    });
+}
 
 function styleManipulator(element, property, value)
 {
